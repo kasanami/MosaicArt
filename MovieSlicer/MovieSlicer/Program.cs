f56d16834d6ef10ada5032f0f6448861f30e7147
笠浪貴大 <dev.kacha@gmail.com>
2022-05-20 22:13:22 +0900

    動画キャプチャ機能追加

--- MovieSlicer/MovieSlicer/Program.cs
+++ MovieSlicer/MovieSlicer/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace MovieSlicer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var capture = new VideoCapture(@"d:\test.mp4"))
+            {
+                var img = new Mat();
+                capture.Read(img);
+                BitmapConverter.ToBitmap(img).Save(@"d:\test.jpg");
+            }
+
+        }
+    }
+}
