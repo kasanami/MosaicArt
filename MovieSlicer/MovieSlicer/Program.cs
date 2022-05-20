50514891621ebb48dd1402d747482903c0a82c53
笠浪貴大 <dev.kacha@gmail.com>
2022-05-20 23:50:11 +0900

    画像をリサイズして保存

--- MovieSlicer/MovieSlicer/Program.cs
+++ MovieSlicer/MovieSlicer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
 
@@ -12,13 +14,21 @@ namespace MovieSlicer
         [STAThread]
         static void Main()
         {
-            using (var capture = new VideoCapture(@"d:\test.mp4"))
+            var path = @".mp4";
+            using (var capture = new VideoCapture(path))
             {
                 var img = new Mat();
-                capture.Read(img);
-                BitmapConverter.ToBitmap(img).Save(@"d:\test.jpg");
+                var frameCount=capture.FrameCount-2;
+                for (int i = 0; i < frameCount; i += 10)
+                {
+                    capture.PosFrames = i;
+                    capture.Read(img);
+                    var bitmap = BitmapConverter.ToBitmap(img);
+                    var resizeBitmap = new Bitmap(bitmap, 16, 9);
+                    resizeBitmap.Save($@"{path}_{i}.png", ImageFormat.Png);
+                    Console.WriteLine($"PosFrames={i}");
+                }
             }
-
         }
     }
 }
