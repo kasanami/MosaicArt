ce116e256c847204c3deb4192bc5e9dcc4073e9a
笠浪貴大 <dev.kacha@gmail.com>
2022-05-21 01:42:39 +0900

    コマンドライン引数に対応

--- MovieSlicer/MovieSlicer/Program.cs
+++ MovieSlicer/MovieSlicer/Program.cs
@@ -12,19 +12,58 @@ namespace MovieSlicer
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            var path = @".mp4";
+            if (args.Length < 4)
+            {
+                Console.WriteLine($"引数が不足しています。");
+                return;
+            }
+            var path = args[0];
+            int interval;
+            int width;
+            int height;
+            if (int.TryParse(args[1], out interval) == false)
+            {
+                Console.WriteLine($"args[1]がint型へ変換できません。");
+                return;
+            }
+            if (int.TryParse(args[2], out width) == false)
+            {
+                Console.WriteLine($"args[2]がint型へ変換できません。");
+                return;
+            }
+            if (int.TryParse(args[3], out height) == false)
+            {
+                Console.WriteLine($"args[3]がint型へ変換できません。");
+                return;
+            }
+            if (interval <= 0)
+            {
+                Console.WriteLine($"{nameof(interval)}が0以下です。");
+                return;
+            }
+            if (width <= 0)
+            {
+                Console.WriteLine($"{nameof(width)}が0以下です。");
+                return;
+            }
+            if (height <= 0)
+            {
+                Console.WriteLine($"{nameof(width)}が0以下です。");
+                return;
+            }
+
             using (var capture = new VideoCapture(path))
             {
                 var img = new Mat();
-                var frameCount=capture.FrameCount-2;
-                for (int i = 0; i < frameCount; i += 10)
+                var frameCount = capture.FrameCount - 1;
+                for (int i = 0; i < frameCount; i += interval)
                 {
                     capture.PosFrames = i;
                     capture.Read(img);
                     var bitmap = BitmapConverter.ToBitmap(img);
-                    var resizeBitmap = new Bitmap(bitmap, 16, 9);
+                    var resizeBitmap = new Bitmap(bitmap, width, height);
                     resizeBitmap.Save($@"{path}_{i}.png", ImageFormat.Png);
                     Console.WriteLine($"PosFrames={i}");
                 }
