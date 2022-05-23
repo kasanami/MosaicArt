5ddd5e0a79f5418c9ca06bbb60b9066c76d7e24a
笠浪貴大 <dev.kacha@gmail.com>
2022-05-23 16:09:26 +0900

    ソリューション名変更

---
+++ MosaicArt/ImageAnalyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿using ImageAnalyzer;
+using System.Drawing;
+
+//var files =Directory.GetFiles(@"C:\Users\bigta\Videos\4K Video Downloader\_");
+//foreach (var file in files)
+var file = @"C:\Users\bigta\Videos\4K Video Downloader\_\20220427 【original animation MV】マリン出航！！【hololive 宝鐘マリン】.mp4_3410.png";
+{
+    var bitmap = new Bitmap(file);
+    var imageInfo = new ImageInfo(bitmap);
+}
