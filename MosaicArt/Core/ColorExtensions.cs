0631cfbe04c1e6eb838ce70030606a8d16dcf1e8
笠浪貴大 <dev.kacha@gmail.com>
2022-05-30 22:48:56 +0900

    LuminanceImage 追加 ColorExtensions 追加

---
+++ MosaicArt/Core/ColorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace MosaicArt.Core
+{
+    /// <summary>
+    /// Colorの拡張関数
+    /// </summary>
+    public static class ColorExtensions
+    {
+        /// <summary>
+        /// 輝度(0～255の値)
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static byte GetLuminance(this Color color)
+        {
+            return (byte)Math.Round(color.R * 0.298912 + color.G * 0.586611 + color.B * 0.114478);
+        }
+    }
+}
---
+++ MosaicArt/Core/LuminanceImage.cs
@@ -0,0 +1,46 @@
+﻿using MessagePack;
+using System.Drawing;
+
+namespace MosaicArt.Core
+{
+#pragma warning disable CA1416 // プラットフォームの互換性を検証
+    /// <summary>
+    /// 輝度画像
+    /// </summary>
+    [MessagePackObject(true)]
+    public class LuminanceImage : BytesImage
+    {
+        /// <summary>
+        /// 1ピクセル1バイト
+        /// </summary>
+        const int PixelSize = 1;
+        public LuminanceImage(Bitmap bitmap)
+        {
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    var color = bitmap.GetPixel(x, y);
+                    Bytes.Add(color.GetLuminance());
+                }
+            }
+        }
+        public LuminanceImage(Bitmap bitmap, int width, int height) : this(bitmap.Resize(width, height))
+        {
+        }
+        public override Color GetPixel(int x, int y)
+        {
+            int offset = ((y * Width) + x);
+            var l = Bytes[offset];
+            return Color.FromArgb(l, l, l);
+        }
+        public override void SetPixel(int x, int y, Color color)
+        {
+            int offset = ((y * Width) + x);
+            Bytes[offset] = color.GetLuminance();
+        }
+    }
+#pragma warning restore CA1416 // プラットフォームの互換性を検証
+}
