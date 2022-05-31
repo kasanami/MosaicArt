c4151ae39dcd315188787ee9bc10f7db0f688c80
笠浪貴大 <dev.kacha@gmail.com>
2022-05-31 23:24:35 +0900

    Argb2222 追加

---
+++ MosaicArt/Core/Argb2222.cs
@@ -0,0 +1,139 @@
+﻿using MessagePack;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace MosaicArt.Core
+{
+    /// <summary>
+    /// 8ビットのRGB情報（各値が赤3ビット、緑3ビット、青2ビット）
+    /// </summary>
+    [MessagePackObject(true)]
+    public struct Argb2222
+    {
+        #region 定数
+        public const int ABitsCount = 2;
+        public const int RBitsCount = 2;
+        public const int GBitsCount = 2;
+        public const int BBitsCount = 2;
+        public const int ABitsShift = RBitsCount + GBitsCount + BBitsCount;
+        public const int RBitsShift = GBitsCount + BBitsCount;
+        public const int GBitsShift = BBitsCount;
+        public const int BBitsShift = 0;
+        public const int ABitsMask = 0b1100_0000;
+        public const int RBitsMask = 0b0011_0000;
+        public const int GBitsMask = 0b0000_1100;
+        public const int BBitsMask = 0b0000_0011;
+        public const int AMax = 3;
+        public const int RMax = 3;
+        public const int GMax = 3;
+        public const int BMax = 3;
+
+        public static readonly Argb2222 Zero = new Argb2222();
+        public static readonly Argb2222 Red = new Argb2222(RMax, 0, 0);
+        public static readonly Argb2222 Green = new Argb2222(0, GMax, 0);
+        public static readonly Argb2222 Blue = new Argb2222(0, 0, BMax);
+        public static readonly Argb2222 White = new Argb2222(RMax, GMax, BMax);
+        public static readonly Argb2222 Black = new Argb2222(0, 0, 0);
+        #endregion 定数
+
+        #region フィールド
+        public byte Bits = 0;
+        #endregion フィールド
+
+        #region プロパティ
+        [IgnoreMember]
+        public int A
+        {
+            get { return (byte)((Bits & ABitsMask) >> ABitsShift); }
+            set { Bits = (byte)((Bits & ~ABitsMask) | (value << ABitsShift) & ABitsMask); }
+        }
+        [IgnoreMember]
+        public int R
+        {
+            get { return (byte)((Bits & RBitsMask) >> RBitsShift); }
+            set { Bits = (byte)((Bits & ~RBitsMask) | (value << RBitsShift) & RBitsMask); }
+        }
+        [IgnoreMember]
+        public int G
+        {
+            get { return (byte)((Bits & GBitsMask) >> GBitsShift); }
+            set { Bits = (byte)((Bits & ~GBitsMask) | (value << GBitsShift) & GBitsMask); }
+        }
+        [IgnoreMember]
+        public int B
+        {
+            get { return (byte)((Bits & BBitsMask) >> BBitsShift); }
+            set { Bits = (byte)((Bits & ~BBitsMask) | (value << BBitsShift) & BBitsMask); }
+        }
+        #endregion プロパティ
+
+        #region コンストラクタ
+        public Argb2222(int a, int r, int g, int b)
+        {
+            A = a;
+            R = r;
+            G = g;
+            B = b;
+        }
+        public Argb2222(int r, int g, int b)
+        {
+            A = AMax;
+            R = r;
+            G = g;
+            B = b;
+        }
+        public Argb2222(Color color)
+        {
+            A = Utility.ElasticityBits8To2Array[color.A];
+            R = Utility.ElasticityBits8To2Array[color.R];
+            G = Utility.ElasticityBits8To2Array[color.G];
+            B = Utility.ElasticityBits8To2Array[color.B];
+        }
+        public Argb2222(Rgb rgb) : this(AMax, (int)Math.Round(rgb.R * RMax), (int)Math.Round(rgb.G * GMax), (int)Math.Round(rgb.B * BMax))
+        {
+        }
+        public Argb2222(byte bits)
+        {
+            Bits = bits;
+        }
+        #endregion コンストラクタ
+
+        #region operator
+        public static Argb2222 operator +(Argb2222 left, Argb2222 right)
+        {
+            return new Argb2222(left.A + right.A, left.R + right.R, left.G + right.G, left.B + right.B);
+        }
+        public static Argb2222 operator -(Argb2222 left, Argb2222 right)
+        {
+            return new Argb2222(left.A - right.A, left.R - right.R, left.G - right.G, left.B - right.B);
+        }
+
+        public static explicit operator Argb2222(Color color)
+        {
+            return new Argb2222(color);
+        }
+        public static implicit operator Color(Argb2222 rgb)
+        {
+            // 3ビットを8ビットに拡張する。(0b00000111 →　0b111_111_11)
+            // 2ビットを8ビットに拡張する。(0b00000011 →　0b11_11_11_11)
+            var a = Utility.ElasticityBits2To8Array[rgb.A];
+            var r = Utility.ElasticityBits2To8Array[rgb.R];
+            var g = Utility.ElasticityBits2To8Array[rgb.G];
+            var b = Utility.ElasticityBits2To8Array[rgb.B];
+            return Color.FromArgb(a, r, g, b);
+        }
+        public static explicit operator Argb2222(Rgb rgb)
+        {
+            return new Argb2222(rgb);
+        }
+        public static implicit operator byte(Argb2222 rgb)
+        {
+            return rgb.Bits;
+        }
+        public static implicit operator Argb2222(byte bits)
+        {
+            return new Argb2222(bits);
+        }
+        #endregion operator
+    }
+}
---
+++ MosaicArt/CoreTests/Argb2222Tests.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MosaicArt.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaicArt.Core.Tests
+{
+    [TestClass()]
+    public class Argb2222Tests
+    {
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Argb2222 argb = new Argb2222(Rgb.Red);
+            Assert.AreEqual(Argb2222.Red, argb);
+            argb = new Argb2222(Rgb.Green);
+            Assert.AreEqual(Argb2222.Green, argb);
+            argb = new Argb2222(Rgb.Blue);
+            Assert.AreEqual(Argb2222.Blue, argb);
+            argb = new Argb2222(Rgb.White);
+            Assert.AreEqual(Argb2222.White, argb);
+            argb = new Argb2222(Rgb.Black);
+            Assert.AreEqual(Argb2222.Black, argb);
+
+            argb = new Argb2222(1, 0, 0, 0);
+            Assert.AreEqual(0b01_00_00_00, argb.Bits);
+            argb = new Argb2222(0, 1, 0, 0);
+            Assert.AreEqual(0b00_01_00_00, argb.Bits);
+            argb = new Argb2222(0, 0, 1, 0);
+            Assert.AreEqual(0b00_00_01_00, argb.Bits);
+            argb = new Argb2222(0, 0, 0, 1);
+            Assert.AreEqual(0b00_00_00_01, argb.Bits);
+
+            argb = new Argb2222(1, 0, 0);
+            Assert.AreEqual(0b11_01_00_00, argb.Bits);
+            argb = new Argb2222(0, 1, 0);
+            Assert.AreEqual(0b11_00_01_00, argb.Bits);
+            argb = new Argb2222(0, 0, 1);
+            Assert.AreEqual(0b11_00_00_01, argb.Bits);
+        }
+        [TestMethod()]
+        public void PropertiesTest()
+        {
+            // 設定した値が正しいかチェック
+            // 他のビットを汚染していないかチェック
+            Argb2222 argb = Argb2222.White;
+            for (int i = 0; i <= Argb2222.AMax; i++)
+            {
+                argb.A = i;
+                Assert.AreEqual(i, argb.A);
+                Assert.AreEqual(Argb2222.RMax, argb.R);
+                Assert.AreEqual(Argb2222.GMax, argb.G);
+                Assert.AreEqual(Argb2222.BMax, argb.B);
+            }
+            for (int i = 0; i <= Argb2222.RMax; i++)
+            {
+                argb.R = i;
+                Assert.AreEqual(i, argb.R);
+                Assert.AreEqual(Argb2222.AMax, argb.A);
+                Assert.AreEqual(Argb2222.GMax, argb.G);
+                Assert.AreEqual(Argb2222.BMax, argb.B);
+            }
+            for (int i = 0; i <= Argb2222.GMax; i++)
+            {
+                argb.G = i;
+                Assert.AreEqual(i, argb.G);
+                Assert.AreEqual(Argb2222.AMax, argb.A);
+                Assert.AreEqual(Argb2222.RMax, argb.R);
+                Assert.AreEqual(Argb2222.BMax, argb.B);
+            }
+            for (int i = 0; i <= Argb2222.BMax; i++)
+            {
+                argb.B = i;
+                Assert.AreEqual(i, argb.B);
+                Assert.AreEqual(Argb2222.AMax, argb.A);
+                Assert.AreEqual(Argb2222.RMax, argb.R);
+                Assert.AreEqual(Argb2222.GMax, argb.G);
+            }
+
+            argb = Argb2222.Zero;
+            for (int i = 0; i <= Argb2222.RMax; i++)
+            {
+                argb.A = i;
+                Assert.AreEqual(i, argb.A);
+                Assert.AreEqual(0, argb.R);
+                Assert.AreEqual(0, argb.G);
+                Assert.AreEqual(0, argb.B);
+            }
+            argb = Argb2222.Zero;
+            for (int i = 0; i <= Argb2222.RMax; i++)
+            {
+                argb.R = i;
+                Assert.AreEqual(0, argb.A);
+                Assert.AreEqual(i, argb.R);
+                Assert.AreEqual(0, argb.G);
+                Assert.AreEqual(0, argb.B);
+            }
+            argb = Argb2222.Zero;
+            for (int i = 0; i <= Argb2222.GMax; i++)
+            {
+                argb.G = i;
+                Assert.AreEqual(0, argb.A);
+                Assert.AreEqual(0, argb.R);
+                Assert.AreEqual(i, argb.G);
+                Assert.AreEqual(0, argb.B);
+            }
+            argb = Argb2222.Zero;
+            for (int i = 0; i <= Argb2222.BMax; i++)
+            {
+                argb.B = i;
+                Assert.AreEqual(0, argb.A);
+                Assert.AreEqual(0, argb.R);
+                Assert.AreEqual(0, argb.G);
+                Assert.AreEqual(i, argb.B);
+            }
+        }
+
+        [TestMethod()]
+        public void OperatorsTest()
+        {
+            Argb2222 argb;
+            for (int i = 0; i <= byte.MaxValue; i++)
+            {
+                argb = (byte)i;
+                byte bits = (byte)argb;
+                Assert.AreEqual(i, bits);
+            }
+
+            {
+                argb = new Argb2222(1, 1, 1, 1);
+                Color color = (Color)argb;
+                Assert.AreEqual(0b01010101, color.A);
+                Assert.AreEqual(0b01010101, color.R);
+                Assert.AreEqual(0b01010101, color.G);
+                Assert.AreEqual(0b01010101, color.B);
+            }
+
+            {
+                argb = new Argb2222(2, 2, 2, 2);
+                Color color = (Color)argb;
+                Assert.AreEqual(0b10101010, color.A);
+                Assert.AreEqual(0b10101010, color.R);
+                Assert.AreEqual(0b10101010, color.G);
+                Assert.AreEqual(0b10101010, color.B);
+            }
+
+            {
+                argb = new Argb2222(3, 3, 3, 3);
+                Color color = (Color)argb;
+                Assert.AreEqual(0b11111111, color.A);
+                Assert.AreEqual(0b11111111, color.R);
+                Assert.AreEqual(0b11111111, color.G);
+                Assert.AreEqual(0b11111111, color.B);
+            }
+
+            {
+                argb = (Argb2222)Color.Red;
+                Color color = (Color)argb;
+                Assert.AreEqual(Color.Red.A, color.A);
+                Assert.AreEqual(Color.Red.R, color.R);
+                Assert.AreEqual(Color.Red.G, color.G);
+                Assert.AreEqual(Color.Red.B, color.B);
+            }
+
+            {
+                argb = (Argb2222)Color.White;
+                Color color = (Color)argb;
+                Assert.AreEqual(Color.White.A, color.A);
+                Assert.AreEqual(Color.White.R, color.R);
+                Assert.AreEqual(Color.White.G, color.G);
+                Assert.AreEqual(Color.White.B, color.B);
+            }
+
+            {
+                argb = (Argb2222)Color.Blue;
+                Color color = (Color)argb;
+                Assert.AreEqual(Color.Blue.A, color.A);
+                Assert.AreEqual(Color.Blue.R, color.R);
+                Assert.AreEqual(Color.Blue.G, color.G);
+                Assert.AreEqual(Color.Blue.B, color.B);
+            }
+        }
+    }
+}
