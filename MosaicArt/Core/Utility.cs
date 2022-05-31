9f2f057683bac18e382c0bda83957bc532e89899
笠浪貴大 <dev.kacha@gmail.com>
2022-05-31 09:06:26 +0900

    CountOne MatchCount 追加

--- MosaicArt/Core/Utility.cs
+++ MosaicArt/Core/Utility.cs
@@ -166,6 +166,28 @@ namespace MosaicArt.Core
         {
             0,85,170,255,
         };
+
+        /// <summary>
+        ///  1 のビットを数える
+        /// </summary>
+        public static int CountOne(uint bits)
+        {
+            bits = (bits & 0x55555555) + (bits >> 1 & 0x55555555);
+            bits = (bits & 0x33333333) + (bits >> 2 & 0x33333333);
+            bits = (bits & 0x0f0f0f0f) + (bits >> 4 & 0x0f0f0f0f);
+            bits = (bits & 0x00ff00ff) + (bits >> 8 & 0x00ff00ff);
+            bits = (bits & 0x0000ffff) + (bits >> 16 & 0x0000ffff);
+            return (int)bits;
+        }
+        /// <summary>
+        /// 2つのビット配列一致しているビットを数える。
+        /// ※1だけではなく0の一致もカウントする。
+        /// </summary>
+        public static int MatchCount(ushort bits0, ushort bits1)
+        {
+            ushort bits = (ushort)(bits0 ^ bits1);
+            return CountOne((ushort)~bits);
+        }
         #endregion ビット操作
     }
 }
--- MosaicArt/CoreTests/UtilityTests.cs
+++ MosaicArt/CoreTests/UtilityTests.cs
@@ -31,5 +31,40 @@ namespace MosaicArt.Core.Tests
             color1 = Color.White;
             Assert.AreEqual(441.672955930063709849498817084, Utility.Distance(color0, color1));
         }
+        [TestMethod()]
+        public void CountOneTest()
+        {
+            Assert.AreEqual(0, Utility.CountOne(0b0));
+            Assert.AreEqual(1, Utility.CountOne(0b1));
+            Assert.AreEqual(1, Utility.CountOne(0b10));
+
+            Assert.AreEqual(2, Utility.CountOne(0b11));
+            Assert.AreEqual(2, Utility.CountOne(0b101));
+
+            Assert.AreEqual(3, Utility.CountOne(0b111));
+            Assert.AreEqual(3, Utility.CountOne(0b1011));
+            Assert.AreEqual(3, Utility.CountOne(0b10101));
+        }
+        [TestMethod()]
+        public void MatchCountTest()
+        {
+            // 全一致
+            Assert.AreEqual(16, Utility.MatchCount(0b0000_0000_0000_0001, 0b0000_0000_0000_0001));
+            Assert.AreEqual(16, Utility.MatchCount(0b0000_0000_0000_0010, 0b0000_0000_0000_0010));
+            Assert.AreEqual(16, Utility.MatchCount(0b0000_0000_0000_0111, 0b0000_0000_0000_0111));
+            Assert.AreEqual(16, Utility.MatchCount(0b0000_0000_0001_0101, 0b0000_0000_0001_0101));
+
+            Assert.AreEqual(0, Utility.MatchCount(0b0000_0000_0000_0000, 0b1111_1111_1111_1111));
+            Assert.AreEqual(1, Utility.MatchCount(0b0000_0000_0000_0000, 0b1111_1111_1111_1110));
+            Assert.AreEqual(2, Utility.MatchCount(0b0000_0000_0000_0000, 0b1111_1111_1011_1011));
+            Assert.AreEqual(3, Utility.MatchCount(0b0000_0000_0000_0000, 0b1111_1111_1101_0110));
+            Assert.AreEqual(4, Utility.MatchCount(0b0000_0000_0000_0000, 0b1111_1111_0101_0110));
+
+            Assert.AreEqual(0, Utility.MatchCount(0b1111_1111_1111_1111, 0b0000_0000_0000_0000));
+            Assert.AreEqual(1, Utility.MatchCount(0b1111_1111_1111_1111, 0b0000_0000_0000_0100));
+            Assert.AreEqual(2, Utility.MatchCount(0b1111_1111_1111_1111, 0b0000_0000_1000_0100));
+            Assert.AreEqual(3, Utility.MatchCount(0b1111_1111_1111_1111, 0b0000_0000_1000_1100));
+            Assert.AreEqual(4, Utility.MatchCount(0b1111_1111_1111_1111, 0b0000_0000_1001_1100));
+        }
     }
 }
