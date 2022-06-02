224c58e5e69937a8281907c3410989a9ce7a2615
笠浪貴大 <dev.kacha@gmail.com>
2022-06-02 21:40:42 +0900

    Rgb332Image Rgb332Image4x4 追加

---
+++ MosaicArt/Core/Rgb332Image4x4.cs
@@ -0,0 +1,38 @@
+﻿using MessagePack;
+using System.Drawing;
+
+namespace MosaicArt.Core
+{
+#pragma warning disable CA1416 // プラットフォームの互換性を検証
+    /// <summary>
+    /// Rgb332かつサイズが4x4固定の画像
+    /// </summary>
+    [MessagePackObject(true)]
+    public class Rgb332Image4x4 : Rgb332Image
+    {
+        const int _Width = 4;
+        const int _Height = 4;
+
+        public override int Width { get; set; } = _Width;
+        public override int Height { get; set; } = _Height;
+
+        public const int BytesSize = PixelSize * _Width * _Height;
+
+        public Rgb332Image4x4()
+        {
+        }
+        public Rgb332Image4x4(Bitmap bitmap)
+        {
+            bitmap = bitmap.Resize(Width, Height);
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    var color = bitmap.GetPixel(x, y);
+                    Bytes.Add(color.GetLuminance());
+                }
+            }
+        }
+    }
+#pragma warning restore CA1416 // プラットフォームの互換性を検証
+}
