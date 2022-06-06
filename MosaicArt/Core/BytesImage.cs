92f7de8a86c6a751fa38d206e20c8d9dd4c50bfb
笠浪貴大 <dev.kacha@gmail.com>
2022-06-06 18:34:37 +0900

    Equals追加

--- MosaicArt/Core/BytesImage.cs
+++ MosaicArt/Core/BytesImage.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 namespace MosaicArt.Core
 {
     [MessagePackObject(true)]
-    public class BytesImage : AbstractImage
+    public class BytesImage : AbstractImage, IEquatable<BytesImage>
     {
         public override int Width { get; set; } = 0;
         public override int Height { get; set; } = 0;
@@ -18,6 +18,41 @@ namespace MosaicArt.Core
         public override void SetPixel(int x, int y, Color color)
         {
             throw new NotImplementedException();
+        }
+        /// <summary>
+        /// バイト配列がすべて同じならtrueを返す。
+        /// </summary>
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as BytesImage);
+        }
+        /// <summary>
+        /// バイト配列がすべて同じならtrueを返す。
+        /// </summary>
+        public bool Equals(BytesImage? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            if (Width != other.Width)
+            {
+                return false;
+            }
+            if (Height != other.Height)
+            {
+                return false;
+            }
+            if (Bytes.Count != other.Bytes.Count)
+            {
+                return false;
+            }
+            return Bytes.SequenceEqual(other.Bytes);
+        }
+
+        public override int GetHashCode()
+        {
+            return Bytes.GetHashCode();
         }
     }
 }
